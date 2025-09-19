d07f847eb21f8b5bb607f7417ad0044b07f89039
Pouria <65418938+NEWPouria@users.noreply.github.com>
2025-09-19 14:17:40 +0330

    Time: 1 ms (58.51%) | Memory: 20.5 MB (68.09%) - LeetSync

---
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.php
@@ -0,0 +1,30 @@
+class Solution {
+    //NEWPouria
+
+    /**
+     * @param String $text
+     * @param String $brokenLetters
+     * @return Integer
+     */
+    function canBeTypedWords($text, $brokenLetters) {
+        
+        $words = explode(' ', $text);
+    $brokenSet = str_split($brokenLetters);
+    $count = 0;
+    
+    foreach ($words as $word) {
+        $canType = true;
+        foreach (str_split($word) as $char) {
+            if (in_array($char, $brokenSet)) {
+                $canType = false;
+                break;
+            }
+        }
+        if ($canType) {
+            $count++;
+        }
+    }
+    
+    return $count;
+    }
+}
